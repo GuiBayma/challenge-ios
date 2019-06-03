282cc86f5d450fcb12cd4d7bbd436f3d1563c8f9
Guilherme Bayma <gfbayma@gmail.com>
2019-06-03 12:26:07 -0300

    Project initial commit

---
+++ Lodjinha/Lodjinha/AppDelegate.swift
@@ -0,0 +1,28 @@
+//
+//  AppDelegate.swift
+//  Lodjinha
+//
+//  Created by Guilherme Bayma on 03/06/19.
+//  Copyright © 2019 Guilherme Bayma. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+
+        window = UIWindow(frame: UIScreen.main.bounds)
+
+        let controller = UIViewController()
+        controller.view.backgroundColor = .cyan
+
+        window?.rootViewController = controller
+        window?.makeKeyAndVisible()
+
+        return true
+    }
+}
